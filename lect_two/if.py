ee73465b27f1f0a1a55a6125140182539892a65d
Katende Chris Marvin <85847700+256-katendechris-256@users.noreply.github.com>
2023-08-09 22:47:22 +0300

    Lecture Two
    conditions an loops

---
+++ lect_two/if.py
@@ -0,0 +1,7 @@
+age =int(input("How old are you "))
+if(age<=12):
+    print("You are a child")
+elif(age>12 and age<=20):
+    print("You are  teenager")
+else: print("you are an adult" )
+    
---
+++ lect_two/loops.py
@@ -0,0 +1,13 @@
+names=["katende","chris","marvin","pro","max"]
+for name in names:
+    print(name)
+
+#while conditions
+x=0
+while x<=10:
+    print(x)
+    x+=1
+
+num=0
+while num<=len(names):
+    print(num)    
